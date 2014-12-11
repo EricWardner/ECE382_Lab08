bf25e26c2459117111b1096e182cb0c1325a0f2b
unknown <C16Eric.Wardner@c16ejwardner.usafa.ds.af.edu>
2014-12-10 23:09:04 -0700

    Bonus Functionality

--- main.c
+++ main.c
@@ -21,14 +21,7 @@ int main(void) {
 
 
 	while(1){
-		// Configure P1.1 to be the ADC input (Left)
 
-//		if (leftSensor() > 0x0200)	P1OUT |= BIT0;
-//		else				P1OUT &= ~BIT0;
-//
-//
-//		if (rightSensor() > 0x0200)	P1OUT |= BIT6;
-//		else					P1OUT &= ~BIT6;
 
 		c = centerSensor();
 		l = leftSensor();
@@ -42,7 +35,7 @@ int main(void) {
 		c = (c+c1+c2+c3)/4;
 		l = (l+l1+l2+l3)/4;
 
-		while(l < 600){
+		while(l < 650){
 			c = centerSensor();
 			l = leftSensor();
 			c1 = centerSensor();
@@ -79,75 +72,5 @@ int main(void) {
 		forward();
 
 
-//		while (c > 600){
-//			c = centerSensor();
-//			c1 = centerSensor();
-//			c2 = centerSensor();
-//			c3 = centerSensor();
-//
-//			c = (c+c1+c2+c3)/4;
-//			P1OUT |= (BIT0|BIT6);
-//			//stop();
-//			//__delay_cycles(LEFT45);
-//			leftTurn();
-//			//__delay_cycles(LEFT45);
-//			//forward();
-//		}
-//		P1OUT &= ~(BIT0|BIT6);
-//
-//		forward();
-//
-//		c = centerSensor();
-//		c1 = centerSensor();
-//		c2 = centerSensor();
-//		c3 = centerSensor();
-//
-//		c = (c+c1+c2+c3)/4;
-//
-//		while (c > 600){
-//			c = centerSensor();
-//			c1 = centerSensor();
-//			c2 = centerSensor();
-//			c3 = centerSensor();
-//
-//			c = (c+c1+c2+c3)/4;
-//			P1OUT |= (BIT0|BIT6);
-//			//stop();
-//			//__delay_cycles(LEFT45);
-//			rightTurn();
-//			//__delay_cycles(LEFT45);
-//			//forward();
-//		}
-
 	}
-
-//	forward();
-//	__delay_cycles(FORWARD);
-//	stop();
-//	__delay_cycles(PAUSE);
-//
-//	back();
-//	__delay_cycles(BACK);
-//	stop();
-//	__delay_cycles(PAUSE);
-//
-//	rightTurn();
-//	__delay_cycles(RIGHT45);
-//	stop();
-//	__delay_cycles(PAUSE);
-//
-//	leftTurn();
-//	__delay_cycles(LEFT45);
-//	stop();
-//	__delay_cycles(PAUSE);
-//
-//	rightTurn();
-//	__delay_cycles(RIGHT90);
-//	stop();
-//	__delay_cycles(PAUSE);
-//
-//	leftTurn();
-//	__delay_cycles(LEFT90);
-//	stop();
-
 } // end main

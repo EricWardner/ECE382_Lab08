1f937e733031a7347bae71735037ca81aa5d1061
unknown <C16Eric.Wardner@c16ejwardner.usafa.ds.af.edu>
2014-12-10 22:18:09 -0700

    required with some edits

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 
 int main(void) {
 
+	unsigned int c, c1, c2, c3;
 	initMotor();
 
 	IFG1=0; 													// clear interrupt flag1
@@ -16,6 +17,8 @@ int main(void) {
 	P1OUT &= ~(BIT0|BIT6);
 
 	forward();
+
+
 	while(1){
 		// Configure P1.1 to be the ADC input (Left)
 
@@ -26,46 +29,83 @@ int main(void) {
 //		if (rightSensor() > 0x0200)	P1OUT |= BIT6;
 //		else					P1OUT &= ~BIT6;
 
+		c = centerSensor();
+		c1 = centerSensor();
+		c2 = centerSensor();
+		c3 = centerSensor();
 
-		if (centerSensor() > 0x0200){
+		c = (c+c1+c2+c3)/4;
+
+
+		while (c > 600){
+			c = centerSensor();
+			c1 = centerSensor();
+			c2 = centerSensor();
+			c3 = centerSensor();
+
+			c = (c+c1+c2+c3)/4;
 			P1OUT |= (BIT0|BIT6);
-			stop();
+			//stop();
+			//__delay_cycles(LEFT45);
 			leftTurn();
-			__delay_cycles(LEFT45);
-			forward();
+			//__delay_cycles(LEFT45);
+			//forward();
 		}
-		else{
-			P1OUT &= ~(BIT0|BIT6);
+		P1OUT &= ~(BIT0|BIT6);
+
+		forward();
+
+		c = centerSensor();
+		c1 = centerSensor();
+		c2 = centerSensor();
+		c3 = centerSensor();
+
+		c = (c+c1+c2+c3)/4;
+
+		while (c > 600){
+			c = centerSensor();
+			c1 = centerSensor();
+			c2 = centerSensor();
+			c3 = centerSensor();
+
+			c = (c+c1+c2+c3)/4;
+			P1OUT |= (BIT0|BIT6);
+			//stop();
+			//__delay_cycles(LEFT45);
+			rightTurn();
+			//__delay_cycles(LEFT45);
+			//forward();
 		}
-		}
 
-	forward();
-	__delay_cycles(FORWARD);
-	stop();
-	__delay_cycles(PAUSE);
+	}
 
-	back();
-	__delay_cycles(BACK);
-	stop();
-	__delay_cycles(PAUSE);
-
-	rightTurn();
-	__delay_cycles(RIGHT45);
-	stop();
-	__delay_cycles(PAUSE);
-
-	leftTurn();
-	__delay_cycles(LEFT45);
-	stop();
-	__delay_cycles(PAUSE);
-
-	rightTurn();
-	__delay_cycles(RIGHT90);
-	stop();
-	__delay_cycles(PAUSE);
-
-	leftTurn();
-	__delay_cycles(LEFT90);
-	stop();
+//	forward();
+//	__delay_cycles(FORWARD);
+//	stop();
+//	__delay_cycles(PAUSE);
+//
+//	back();
+//	__delay_cycles(BACK);
+//	stop();
+//	__delay_cycles(PAUSE);
+//
+//	rightTurn();
+//	__delay_cycles(RIGHT45);
+//	stop();
+//	__delay_cycles(PAUSE);
+//
+//	leftTurn();
+//	__delay_cycles(LEFT45);
+//	stop();
+//	__delay_cycles(PAUSE);
+//
+//	rightTurn();
+//	__delay_cycles(RIGHT90);
+//	stop();
+//	__delay_cycles(PAUSE);
+//
+//	leftTurn();
+//	__delay_cycles(LEFT90);
+//	stop();
 
 } // end main
